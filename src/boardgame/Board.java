5e0c5bbb4fd2ca80d769bd04c75c799761574f0e
bene <matheusbenediht@gmail.com>
2023-07-07 19:11:49 -0300

    Acidionando metodo There is a piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,4 +47,7 @@ public class Board {
         return positionExists(position.getRow(), position.getColumn());
     }
 
+    public boolean thereIsAPiece(Position position){
+        return piece(position) != null;
+    }
 }
