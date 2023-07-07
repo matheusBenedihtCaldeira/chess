4dda3b1ca6c379d5a02e82fa394d40ab010fc131
bene <matheusbenediht@gmail.com>
2023-07-07 19:10:46 -0300

    Implementando metodo positionExists

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -40,4 +40,11 @@ public class Board {
         piece.position = position;
     }
 
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
 }
